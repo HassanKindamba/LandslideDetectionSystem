ca8a089c4e1b9f097ece1a4da34651aeae5dcc13
HassanKindamba <kindambahassani2005@gmail.com>
2026-08-01 13:53:33 +0300

    enable api routes and exclude sensor endpoints from csrf check

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -7,11 +7,16 @@ use Illuminate\Foundation\Configuration\Middleware;
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
+        api: __DIR__.'/../routes/api.php', // *** Tumeongeza hii ili routes/api.php ifanye kazi ***
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        // *** kuondoa CSRF requirement kwa ESP32 ***
+        $middleware->validateCsrfTokens(except: [
+            'api/sensor-data',
+            'sensor-data',
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
